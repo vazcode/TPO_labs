32ac9447e060c96fb02571921e937189b1fb0b26
Pavel Vazhui <101338523+vazcode@users.noreply.github.com>
2022-03-19 11:48:47 +0100

    beta test

---
+++ Lab1_TPO/src/FExcepts.java
@@ -0,0 +1,11 @@
+public class FExcepts extends Exception{
+    public FExcepts(){}
+    public FExcepts(String message)
+    {
+        super (message);
+    }
+    public FExcepts(Throwable cause)
+    {
+        super(cause);
+    }
+}
